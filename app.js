615d7b5fe7c57c13336aaa58f293b090d9c80249
Scott Diemer <sdiemer@cfl.rr.com>
2018-07-05 22:12:55 -0400

    corrected port to work with heroku

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('Server running on port 3000!');
 });
